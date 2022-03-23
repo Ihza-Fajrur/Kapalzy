622514622186dd10e9e7c15bb3df6c5d7e2c525f
HRFI-B <84057899+Ihza-Fajrur@users.noreply.github.com>
2022-03-23 20:12:25 +0700

    Kapalzy init

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Menu_Awal from './screens/Menu_Awal';
+import Menu_Detail_Pemesanan from './screens/Menu_Detail_Pemesanan';
+import Menu_Konfirmasi_Pesanan from './screens/Menu_Konfirmasi_Pesanan';
+import Daftar_Pesanan from './screens/Daftar_Pesanan';
+import Daftar_Pembatalan from './screens/Daftar_Pembatalan';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Menu_Awal' screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Menu_Awal" component={Menu_Awal} />
+        {/* <Stack.Screen name="Menu_Konfirmasi_Pesanan" component={Menu_Konfirmasi_Pesanan} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
